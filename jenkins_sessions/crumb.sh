a726b98a69eb0499d28ab26f7ebd9117d8bca64a
ahmedtawfeek <44138882+ahmedtawfeek@users.noreply.github.com>
2024-01-29 23:18:53 +0200

    Update crumb.sh

--- jenkins_sessions/crumb.sh
+++ jenkins_sessions/crumb.sh
@@ -1,4 +1,5 @@
 #/bin/bash
 
 CRUMB=$(curl --user admin:11eb1a8f9bc0f24e427978215a6ae77c45 http://192.168.1.106:8080/crumbIssuer/api/xml?xpath=concat\(//crumbRequestField,%22:%22,//crumb\))
-curl --user "admin:11eb1a8f9bc0f24e427978215a6ae77c45" -H "$CRUMB" -X POST http://192.168.1.106:8080/job/first-job/build
+#curl --user "admin:11eb1a8f9bc0f24e427978215a6ae77c45" -H "$CRUMB" -X POST http://192.168.1.106:8080/job/first-job/build
+curl --user "admin:11eb1a8f9bc0f24e427978215a6ae77c45" -H "$CRUMB" -X POST http://192.168.1.106:8080/job/first-job/buildWithParameters? -F Ahmed="emad"
